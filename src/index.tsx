aa2382e7bf8dfdf6344fc8804b031c2c1cf0fce0
Vignesh Hegde <hegdevignesh54@gmail.com>
2026-07-07 18:42:25 +0530

    TODO: Create Resusable Components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import "@/globals.css";
 import { Text, View } from "react-native";
 
-export default function index() {
+export default function Index() {
 	return (
 		<View>
 			<Text>index</Text>
